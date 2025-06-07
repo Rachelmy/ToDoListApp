82299a3e5cb9ed850416024179869307bc53bcf9
Qiuling <52867597+QiuUMN@users.noreply.github.com>
2025-06-07 15:09:22 -0700

    feat: using pydantic to handle request validation

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# Define the Item model that clients will use to send data
+class Item(BaseModel):
+    text: str   #required
+    is_done: bool = False
 
 # Root endpoint to verify the app is running
 @app.get("/")
